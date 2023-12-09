7be586027d84c316b7a7242b52c807ba23799d46
Tom Smith <tsmith@opera.com>
2023-12-09 23:05:56 +0000

    Day4 Part1 Get card score

--- day-04/src/card.rs
+++ day-04/src/card.rs
@@ -37,10 +37,25 @@ impl Card {
             player_numbers,
         }
     }
+
+    fn get_no_of_winning_numbers(&self) -> u8 {
+        self.player_numbers
+            .intersection(&self.winning_numbers)
+            .count() as u8
+    }
+
+    pub fn get_score(&self) -> u32 {
+        let no_of_winning_numbers = self.get_no_of_winning_numbers();
+        if no_of_winning_numbers == 0 {
+            return 0;
+        }
+        2_u32.pow(no_of_winning_numbers as u32 - 1)
+    }
 }
 
 #[cfg(test)]
 mod a_card {
+    use rstest::rstest;
     use std::collections::HashSet;
 
     use super::Card;
@@ -73,5 +88,17 @@ mod a_card {
         let actual_card = Card::from_line(line);
 
         assert_eq!(expected_card, actual_card);
+    }
+
+    #[rstest]
+    #[case("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8)]
+    #[case("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2)]
+    #[case("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2)]
+    #[case("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1)]
+    #[case("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0)]
+    #[case("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0)]
+    fn can_get_its_score(#[case] line: &str, #[case] score: u32) {
+        let card = Card::from_line(line);
+        assert_eq!(score, card.get_score());
     }
 }
