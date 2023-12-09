5c62cf5e3b1024b605f54be7bb8e7dd372f38eaa
Tom Smith <tsmith@opera.com>
2023-12-09 23:11:00 +0000

    Day4 Part1 Answer

--- day-04/src/bin/part1.rs
+++ day-04/src/bin/part1.rs
@@ -1,5 +1,18 @@
+use std::{env, fs};
+
 use day_04::card::Card;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+    println!("Reading from file '{}'", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let mut sum = 0;
+    contents
+        .lines()
+        .into_iter()
+        .for_each(|line| sum += Card::from_line(line).get_score());
+    println!("Result = {}", sum);
 }
