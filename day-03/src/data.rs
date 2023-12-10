916040d236ee00e848836b3556dccb72133a8e5e
Tom Smith <tsmith@opera.com>
2023-12-10 16:51:43 +0000

    Day3 Part1 Change of approach
    Instead of iterating over our symbols and trying to find every adjacent part number, lets parse all our part numbers first, and then check each one for an adjacent symbol

--- day-03/src/data.rs
+++ day-03/src/data.rs
@@ -8,6 +8,8 @@ use std::{
 pub struct EngineSchematic {
     data: Vec<char>,
     size: (u8, u8),
+    part_numbers: Vec<PartNumber>,
+    symbols: Vec<Symbol>,
 }
 
 #[derive(Debug, Clone)]
@@ -32,11 +34,102 @@ impl EngineSchematic {
             bail!(SizeError);
         }
 
-        Ok(EngineSchematic { data, size })
+        let mut part_numbers = vec![];
+        let mut symbols = vec![];
+        let mut current_part_indices = vec![];
+
+        let mut i: u32 = 0;
+        while i < data.len() as u32 {
+            let (column, row) = Self::index_to_coordinate(i, &size);
+            let c = data.get(i as usize).expect("Data out of range");
+            if (!Self::char_is_part_number(c) || i % size.0 as u32 == 0)
+                && !current_part_indices.is_empty()
+            {
+                part_numbers.push(Self::create_part_number_from_indices(
+                    row,
+                    &current_part_indices,
+                    &size,
+                    &data,
+                ));
+                current_part_indices.clear();
+            }
+            if Self::char_is_part_number(c) {
+                current_part_indices.push(i);
+            } else if Self::char_is_symbol(c) {
+                symbols.push(Symbol {
+                    symbol: c.clone(),
+                    row,
+                    column,
+                });
+            }
+            i += 1;
+        }
+
+        if !current_part_indices.is_empty() {
+            // Check for any remaining part numbers
+            let row = Self::index_to_coordinate(i, &size).1;
+            part_numbers.push(Self::create_part_number_from_indices(
+                row,
+                &current_part_indices,
+                &size,
+                &data,
+            ));
+            current_part_indices.clear();
+        }
+
+        Ok(EngineSchematic {
+            data,
+            size,
+            part_numbers,
+            symbols,
+        })
+    }
+
+    fn create_part_number_from_indices(
+        row: u8,
+        indices: &Vec<u32>,
+        size: &(u8, u8),
+        data: &Vec<char>,
+    ) -> PartNumber {
+        let columns = indices
+            .iter()
+            .map(|ind| Self::index_to_coordinate(*ind, &size).1)
+            .collect();
+        PartNumber {
+            number: Self::indices_to_string(indices, &data)
+                .parse()
+                .expect("Error parsing part number"),
+            row,
+            columns,
+        }
+    }
+
+    fn char_is_part_number(c: &char) -> bool {
+        c.is_numeric()
+    }
+
+    fn char_is_symbol(c: &char) -> bool {
+        !Self::char_is_part_number(c) && c != &'.'
     }
 
     fn in_bounds(&self, cell: &(u8, u8)) -> bool {
         cell.0 < self.size.0 && cell.1 < self.size.1
+    }
+
+    fn index_to_coordinate(index: u32, size: &(u8, u8)) -> (u8, u8) {
+        let height = index / size.0 as u32;
+        let width = index % size.0 as u32;
+        (width as u8, height as u8)
+    }
+
+    fn indices_to_string(indices: &Vec<u32>, data: &Vec<char>) -> String {
+        indices
+            .iter()
+            .map(|i| {
+                data.get(*i as usize)
+                    .expect("Error creating string from indices")
+            })
+            .collect()
     }
 
     pub fn at(&self, cell: &(u8, u8)) -> Result<&char> {
@@ -45,117 +138,6 @@ impl EngineSchematic {
         }
         let index = (self.size.0 * cell.1) as usize + cell.0 as usize;
         Ok(self.data.get(index).expect("Index was not in bounds"))
-    }
-
-    pub fn get_adjacent_part_numbers(&self, cell: (u8, u8)) -> Vec<PartNumber> {
-        let mut part_numbers: Vec<PartNumber> = vec![];
-        let shared_adjacent_cells_to_check =
-            Rc::new(RefCell::new(self.get_adjacent_cell_indexes(&cell)));
-        let binding = Rc::clone(&shared_adjacent_cells_to_check);
-        let binding = binding.borrow();
-        let mut iter = binding.iter();
-        while iter.len() > 0 {
-            let cell = match iter.next() {
-                None => break,
-                Some(val) => val,
-            };
-            let binding = Rc::clone(&shared_adjacent_cells_to_check);
-            let mut adjacent_cells = binding.borrow_mut();
-            adjacent_cells.retain(|check| check != cell);
-            let binding = Rc::clone(&shared_adjacent_cells_to_check);
-            let part_number = match self.get_part_number(cell, binding) {
-                None => continue,
-                Some(pn) => pn,
-            };
-            part_numbers.push(part_number);
-        }
-        part_numbers
-    }
-
-    fn get_adjacent_cell_indexes(&self, cell: &(u8, u8)) -> Vec<(u8, u8)> {
-        let mut cell_indexes = vec![];
-        let offsets: Vec<(i8, i8)> = vec![
-            (-1, -1),
-            (0, -1),
-            (1, -1),
-            (-1, 0),
-            (1, 0),
-            (-1, 1),
-            (0, 1),
-            (1, 1),
-        ];
-        for offset in offsets {
-            let cell_index = match Self::try_get_cell_from_offset(cell, &offset) {
-                Some(val) => val,
-                None => continue,
-            };
-            if !self.in_bounds(&cell_index) {
-                continue;
-            }
-            cell_indexes.push(cell_index);
-        }
-        cell_indexes
-    }
-
-    fn try_get_cell_from_offset(cell: &(u8, u8), offset: &(i8, i8)) -> Option<(u8, u8)> {
-        let x = cell.0 as i8 + offset.0;
-        let y = cell.1 as i8 + offset.1;
-        if x < 0 || y < 0 {
-            return None;
-        }
-        Some((x as u8, y as u8))
-    }
-
-    fn get_part_number(
-        &self,
-        cell: &(u8, u8),
-        cells_to_check: Rc<RefCell<Vec<(u8, u8)>>>,
-    ) -> Option<PartNumber> {
-        let value = self.get_part_number_string_from_cell(cell);
-        let value = match value {
-            None => return None,
-            Some(val) => val,
-        };
-
-        let mut cells_to_check = cells_to_check.borrow_mut();
-        cells_to_check.retain(|check| check != cell);
-        let mut part_number_string = value.to_string();
-        let mut columns = vec![cell.1];
-
-        if let Some(offset_cell) = Self::try_get_cell_from_offset(cell, &(-1, 0)) {
-            if let Some(part_char) = self.get_part_number_string_from_cell(&offset_cell) {
-                part_number_string.insert(0, part_char);
-                cells_to_check.retain(|check| check != cell);
-                columns.insert(0, offset_cell.1);
-            }
-        }
-
-        let number = part_number_string
-            .parse::<u8>()
-            .expect("Error parsing part_number to u8");
-        Some(PartNumber {
-            number,
-            row: cell.0,
-            columns: columns,
-        })
-    }
-
-    fn get_part_number_string_from_cell(&self, cell: &(u8, u8)) -> Option<char> {
-        if !self.in_bounds(cell) {
-            return None;
-        }
-        let value = self.at(cell);
-        let value = match value {
-            Err(err) => {
-                println!("{}", err);
-                return None;
-            }
-            Ok(val) => val,
-        };
-        if !value.is_numeric() {
-            return None;
-        }
-        Some(*value)
     }
 }
 
@@ -189,28 +171,7 @@ mod an_engine_schematic {
         };
         assert_eq!(data, schematic.data);
         assert_eq!(size, schematic.size);
-    }
-
-    #[test]
-    fn can_get_adjacent_part_numbers() {
-        let data = vec!['.', '&', '.', '1', '2', '.', '.', '.', '6'];
-        let size: (u8, u8) = (3, 3);
-        let expected_part_number = PartNumber {
-            number: 12,
-            row: 1,
-            columns: vec![0, 1],
-        };
-
-        let schematic = match EngineSchematic::new(data.clone(), size.clone()) {
-            Ok(sch) => sch,
-            Err(err) => panic!("{}", err),
-        };
-
-        let part_numbers = schematic.get_adjacent_part_numbers((0, 1));
-        let number = part_numbers.first().unwrap();
-        // assert_eq!(expected_part_number.number, number.number);
-        // assert_eq!(expected_part_number.row, number.row);
-        // assert_eq!(expected_part_number.columns, number.columns);
-        assert_eq!(&expected_part_number, number);
+        assert_eq!(2, schematic.part_numbers.len());
+        assert_eq!(1, schematic.symbols.len());
     }
 }
