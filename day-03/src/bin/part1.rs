d894e35705e70f4ac946acbb41d7e409298998e7
Tom Smith <tsmith@opera.com>
2023-12-10 18:03:40 +0000

    Day3 Part1 Add failing sum test

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -22,3 +22,30 @@ fn main() {
         .sum();
     println!("Result = {}", sum);
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use day_03::data::EngineSchematic;
+
+    #[test]
+    fn can_sum_engine_parts() {
+        let file_path = "res/test_data.txt";
+        let contents =
+            fs::read_to_string(file_path).expect("Should have been able to read the file");
+        let height = contents.lines().count();
+        let width = (contents.len() / height) as u8;
+        let height = height as u8;
+        let contents = contents.replace("\n", "");
+        let schematic = EngineSchematic::new(contents.chars().collect(), (width, height))
+            .expect("Failed creating schematic from input");
+        let sum: u32 = schematic
+            .part_numbers
+            .iter()
+            .filter(|part| part.is_adjacent_to_symbol(&schematic))
+            .map(|part| part.number as u32)
+            .sum();
+        assert_eq!(4361, sum);
+    }
+}
