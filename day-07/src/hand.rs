ab63e6d4946b773daf2580d79576412a65d3d51a
Tom Smith <tsmith@opera.com>
2023-12-16 23:27:20 +0000

    Add HandScore and Hand comparison
    Still need to implement HandScore from Cards

--- day-07/src/hand.rs
+++ day-07/src/hand.rs
@@ -1,17 +1,52 @@
 use super::card::Card;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Hand {
     cards: [Card; 5],
+    score: HandScore,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum HandScore {
+    FiveOfAKind,
+    FourOfAKind,
+    FullHouse,
+    ThreeOfAKind,
+    TwoPair,
+    OnePair,
+    HighCard,
 }
 
 impl Hand {
     pub fn new(cards: [Card; 5]) -> Self {
-        Hand { cards }
+        let score = HandScore::from(cards);
+        Hand { score, cards }
     }
 
     pub fn cards(&self) -> &[Card; 5] {
         &self.cards
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.score.cmp(&other.score) {
+            core::cmp::Ordering::Equal => {}
+            ord => return ord,
+        }
+        for card_pair in self.cards.iter().zip(other.cards.iter()) {
+            match card_pair.0.cmp(card_pair.1) {
+                core::cmp::Ordering::Equal => {}
+                ord => return ord,
+            }
+        }
+        core::cmp::Ordering::Equal
     }
 }
 
@@ -32,5 +67,11 @@ impl TryFrom<[char; 5]> for Hand {
             }
         };
         Ok(Hand::new(cards))
+    }
+}
+
+impl From<[Card; 5]> for HandScore {
+    fn from(value: [Card; 5]) -> Self {
+        todo!()
     }
 }
