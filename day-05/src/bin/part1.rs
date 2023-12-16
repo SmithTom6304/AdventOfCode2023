85aeccbe303183de834dfe9ef0843e63bc87843d
Tom Smith <tsmith@opera.com>
2023-12-16 14:33:00 +0000

    Day 5 Part 1 Parsing - Incorrect Answer

--- day-05/src/bin/part1.rs
+++ day-05/src/bin/part1.rs
@@ -1,6 +1,62 @@
-use day_05::item_map::ItemMap;
+use std::{collections::VecDeque, env, fs};
 
-fn main() {}
+use day_05::item_map::{ItemMap, ItemMapEntry};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+    println!("Reading from file '{}'", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let mut lines = contents.lines();
+
+    // Get seeds
+    let seed_line = lines.next().expect("Input had no seed line");
+    let seed_line = &seed_line[7..];
+    let seeds: Vec<u64> = seed_line
+        .split_whitespace()
+        .map(|seed| seed.parse::<u64>().expect("Could not parse seed as u64"))
+        .collect();
+    _ = lines.next();
+
+    let mut maps = VecDeque::<ItemMap>::new();
+    let line = lines.next().unwrap();
+    let mut map_id = &line[..line.len() - 1];
+    let mut current_map = ItemMap {
+        id: map_id.to_string(),
+        entries: vec![],
+    };
+    while let Some(line) = lines.next() {
+        if line.contains("map") {
+            maps.push_back(current_map);
+            map_id = &line[..line.len() - 1];
+            current_map = ItemMap {
+                id: map_id.to_string(),
+                entries: vec![],
+            };
+        } else if line.len() == 0 {
+            continue;
+        } else {
+            current_map.entries.push(ItemMapEntry::from_line(line));
+        }
+    }
+
+    let lowest_location_number = seeds
+        .iter()
+        .map(|seed| map_continuous(&maps, seed))
+        .min()
+        .expect("Map was empty");
+    print!("Result = {}", lowest_location_number);
+}
+
+fn map_continuous(maps: &VecDeque<ItemMap>, seed: &u64) -> u64 {
+    let mut result = *seed;
+    for map in maps.iter() {
+        result = ItemMap::get(&map.entries, &result);
+    }
+    result
+}
 
 #[cfg(test)]
 mod tests {
@@ -12,7 +68,7 @@ mod tests {
     #[case(14, 14)]
     #[case(55, 57)]
     #[case(13, 13)]
-    fn can_map_seeds_to_soil(#[case] seed: u8, #[case] soil: u8) {
+    fn can_map_seeds_to_soil(#[case] seed: u64, #[case] soil: u64) {
         let mut mappings = vec![];
         mappings.push(ItemMapEntry {
             destination_range_start: 50,
--- day-05/src/item_map.rs
+++ day-05/src/item_map.rs
@@ -1,13 +1,44 @@
+#[derive(Debug)]
 pub struct ItemMapEntry {
-    pub destination_range_start: u8,
-    pub source_range_start: u8,
-    pub range_length: u8,
+    pub destination_range_start: u64,
+    pub source_range_start: u64,
+    pub range_length: u64,
 }
 
-pub struct ItemMap {}
+impl ItemMapEntry {
+    pub fn from_line(line: &str) -> Self {
+        let mut parts = line.split_whitespace();
+        let destination_range_start = parts
+            .next()
+            .expect("No destination range start provided")
+            .parse::<u64>()
+            .expect("Could not parse destination range start as u64");
+        let source_range_start = parts
+            .next()
+            .expect("No source range start provided")
+            .parse::<u64>()
+            .expect("Could not source range start as u64");
+        let range_length = parts
+            .next()
+            .expect("No range length provided")
+            .parse::<u64>()
+            .expect("Could not parse range length as u64");
+        Self {
+            destination_range_start,
+            source_range_start,
+            range_length,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ItemMap {
+    pub id: String,
+    pub entries: Vec<ItemMapEntry>,
+}
 
 impl ItemMap {
-    pub fn get(mappings: &Vec<ItemMapEntry>, item: &u8) -> u8 {
+    pub fn get(mappings: &Vec<ItemMapEntry>, item: &u64) -> u64 {
         for map in mappings.iter() {
             if map.source_range_start <= *item && *item < map.source_range_start + map.range_length
             {
