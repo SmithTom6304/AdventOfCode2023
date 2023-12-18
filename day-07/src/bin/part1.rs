ef643fb9248dc1d26735c0ae8ce33329ad1dc770
Tom Smith <tsmith@opera.com>
2023-12-18 12:07:03 +0000

    Day 7 Part 1 Answer

--- day-07/src/bin/part1.rs
+++ day-07/src/bin/part1.rs
@@ -1,10 +1,44 @@
+use std::{env, fs};
+
+use day_07::play::Play;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+    println!("Reading from file '{}'", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let plays: Vec<Play> = match contents
+        .lines()
+        .map(|input| Play::try_from(input))
+        .collect()
+    {
+        Ok(play) => play,
+        Err(err) => panic!("{}", err),
+    };
+
+    let result = calculate_sum_of_scores(plays);
+    println!("Result = {}", result);
+}
+
+fn calculate_sum_of_scores(mut plays: Vec<Play>) -> u32 {
+    plays.sort();
+    let mut rank = 0;
+    let mut sum = 0;
+    let mut iter = plays.into_iter();
+    while let Some(play) = iter.next() {
+        rank += 1;
+        sum += play.bid() * rank;
+    }
+    sum
 }
 
 #[cfg(test)]
 mod tests {
     use day_07::play::Play;
+
+    use crate::calculate_sum_of_scores;
 
     #[test]
     fn example() {
@@ -14,14 +48,8 @@ mod tests {
             Ok(play) => play,
             Err(err) => panic!("{}", err),
         };
-        plays.sort();
-        let mut rank = 0;
-        let mut sum = 0;
-        let mut iter = plays.into_iter();
-        while let Some(play) = iter.next() {
-            rank += 1;
-            sum += play.bid() * rank;
-        }
+
+        let sum = calculate_sum_of_scores(plays);
 
         assert_eq!(6440, sum);
     }
